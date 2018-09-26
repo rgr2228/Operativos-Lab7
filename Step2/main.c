90fa9b8c724a0f1e9dfa16978c4de0ba437bc80b
dugol <danielugiraldo@gmail.com>
2018-09-26 14:32:01 -0500

    Add condition to command line parameters

--- Step2/main.c
+++ Step2/main.c
@@ -2,12 +2,26 @@
 
 int main(int argc, char const *argv[])
 {
+    char l[] = "-l";
+    if (strcmp("-l", argv[1]) == 0)
+    {
 
         int index;
         for (index = 2; index < argc; ++index)
         {
             getProcessInformation(argv[index]);
         }
+    }
+    else
+    {
+        if (argc > 2)
+        {
+            printf("Invalid format command line.\n");
+        }
+        else{
+            getProcessInformation(argv[1]);
+        }
+    }
 
     /*printf("%s",(pi.processName));
     printf("%s",(pi.processState));
